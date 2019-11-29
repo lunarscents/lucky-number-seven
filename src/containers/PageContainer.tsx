503b7398fe4246a0f08ccd21eec8f37d9061bdff
lunarscents <lunarscents7@gmail.com>
2019-11-29 18:51:21 +0900

    [containers] - write PageContainer

---
+++ src/containers/PageContainer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageContainer: React.FC = ({ children }) => {
+  // TODO - add navigation or head
+
+  return <>{children}</>;
+};
+
+export default PageContainer;
