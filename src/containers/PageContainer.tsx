26465d8eeb61cc8e3950c552cf29db73ec29eea3
lunarscents <lunarscents7@gmail.com>
2019-12-03 22:35:51 +0900

    [containers] - rewrite a PageContainer

--- src/containers/PageContainer.tsx
+++ src/containers/PageContainer.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+import styled from "styled-components/native";
+
+const Container = styled.ScrollView`
+  background-color: #eee;
+`;
+
 const PageContainer: React.FC = ({ children }) => {
   // TODO - add navigation or head
 
-  return <>{children}</>;
+  return <Container>{children}</Container>;
 };
 
 export default PageContainer;
