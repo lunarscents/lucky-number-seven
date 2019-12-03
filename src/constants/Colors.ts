030bfc5bc14685663eb0fd93febfd6f81d744003
lunarscents <lunarscents7@gmail.com>
2019-12-03 22:31:24 +0900

    [constants] - change Colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,5 +1,5 @@
-export const BG_COLOR = "black";
-export const TINT_COLOR = "white";
+export const BG_COLOR = "#eeeeee";
+export const TINT_COLOR = "#191919";
 export const GREY_COLOR = "#bdc3c7";
 export const INACTIVE_COLOR = "#7f8c8d";
 export const ACTIVE_COLOR = TINT_COLOR;
